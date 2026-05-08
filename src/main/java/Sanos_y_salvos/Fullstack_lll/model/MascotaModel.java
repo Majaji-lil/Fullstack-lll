ef8cdb52b6ec09d2d37a207747b630bc7533d791
CristianEspinozaSilva <cespinozas@buk.cl>
2026-05-08 15:46:11 -0400

    Agregando cambios generales para que el proyecto inicie de manera correcta y ajustes en la base de datos base que se ocupara

--- src/main/java/Sanos_y_salvos/Fullstack_lll/model/MascotaModel.java
+++ src/main/java/Sanos_y_salvos/Fullstack_lll/model/MascotaModel.java
@@ -12,25 +12,15 @@ public class MascotaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
     private long id;
-
     private String nombre;
     private String especie;
     private String raza;
     private String color_caracteristica;
-<<<<<<< HEAD
-    private String tamanio;
-=======
     private String tamano;
->>>>>>> 2d4fc8e03c4d9f49b74af8791825656b584bc3ac
-
-
     public MascotaModel() {}
 
     public Long getId () { return id; }
-
 
     public String getNombre() {return nombre;}
     public void setNombre(String nombre) {this.nombre = nombre;}
@@ -43,13 +33,7 @@ public class MascotaModel {
 
     public String getColor_caracteristica() {return color_caracteristica;}
     public void setColor_caracteristica(String color_caracteristica) {this.color_caracteristica = color_caracteristica;}
-
-<<<<<<< HEAD
-    public String getTamanio() {return tamanio;}
-    public void setTamanio(String tamaño) {this.tamanio = tamanio;}
-=======
     public String getTamano() {return tamano;}
     public void setTamano(String tamano) {this.tamano = tamano;}
->>>>>>> 2d4fc8e03c4d9f49b74af8791825656b584bc3ac
 
 }
