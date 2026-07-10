311d58024a353cb4018f0f06fc5095dadf45fa47
TheBlindKat <113479485+TheBlindKat@users.noreply.github.com>
2026-07-10 11:49:00 -0400

    fix: agregando urls independientes en rama dev

--- front/frontend-app/src/api/urls.js
+++ front/frontend-app/src/api/urls.js
@@ -1,9 +1,6 @@
 // src/api/urls.js
 
-// Si existen las variables de Render las usa, si no, usa el localhost correspondiente
 export const API_USUARIOS = import.meta.env.VITE_API_USUARIOS || 'http://localhost:8090/api/usuarios';
 export const API_MASCOTAS = import.meta.env.VITE_API_MASCOTAS || 'http://localhost:8090/api/mascotas';
 export const API_REPORTES = import.meta.env.VITE_API_REPORTES || 'http://localhost:8090/api/reportes';
-
-// Mantén esta si tus componentes la usan para cargar imágenes o URL base
 export const API_BASE = import.meta.env.VITE_API_MASCOTAS || 'http://localhost:8090';
